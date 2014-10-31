b55a6f2be4c11f6c7503bfbcef65ddfe3b962b72
hellspawn14 <hellspawn@192.168.0.26>
2014-10-31 17:58:04 -0500

    Shape

---
+++ co.edu.uniandes.picture.webpicture/src/co/edu/uniandes/picture/webpicture/graphical/shapes/Shape.java
@@ -0,0 +1,89 @@
+package co.edu.uniandes.picture.webpicture.graphical.shapes;
+
+import co.edu.uniandes.picture.webpicture.graphical.style.Border;
+import co.edu.uniandes.picture.webpicture.graphical.style.Color;
+
+/**
+ * Representa una forma basica 
+ * @author hellspawn
+ */
+public abstract class Shape extends Graph 
+{
+
+	//------------------------------------------------------------------
+	//Atributos
+	//------------------------------------------------------------------
+	
+	/**
+	 * Color de la forma 
+	 */
+	private Color color; 
+	
+	/**
+	 * Borde de la forma 
+	 */
+	private Border border;
+
+	//------------------------------------------------------------------
+	//Atributos
+	//------------------------------------------------------------------
+
+	/**
+	 * Crea una nueva forma basica con los parametros dados 
+	 * @param height - Alto 
+	 * @param width - Ancho 
+	 * @param x - Posicion inicial en X
+	 * @param y - Posicion inicial en Y
+	 * @param label - Label para mostrar 
+	 * @param color - Color de la forma (fondo)
+	 * @param border - Borde de la forma
+	 */
+	public Shape(int height, int width, int x, int y, String label, Color color, Border border) 
+	{
+		super(height, width, x, y, label);
+		this.color = color;
+		this.border = border;
+	}
+	
+	//------------------------------------------------------------------
+	//Metodos
+	//------------------------------------------------------------------
+
+	/**
+	 * Retorna el script generado para pintar la forma 
+	 * @return - Script generado para pintar la forma
+	 */
+	public abstract String generateScript();
+	
+	//------------------------------------------------------------------
+	//Getters & Setters
+	//------------------------------------------------------------------
+
+	/**
+	 * @return the color
+	 */
+	public Color getColor() {
+		return color;
+	}
+
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	/**
+	 * @return the border
+	 */
+	public Border getBorder() {
+		return border;
+	}
+
+	/**
+	 * @param border the border to set
+	 */
+	public void setBorder(Border border) {
+		this.border = border;
+	}
+}
