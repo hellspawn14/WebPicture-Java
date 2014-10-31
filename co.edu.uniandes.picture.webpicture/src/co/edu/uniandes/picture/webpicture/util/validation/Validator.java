c64a0e6caab3785a66d7f2a3b58e709c669e8efc
hellspawn14 <hellspawn@MacBook-Pro-de-Andres.local>
2014-10-31 03:38:28 -0500

    All

---
+++ co.edu.uniandes.picture.webpicture/src/co/edu/uniandes/picture/webpicture/util/validation/Validator.java
@@ -0,0 +1,41 @@
+package co.edu.uniandes.picture.webpicture.util.validation;
+
+import co.edu.uniandes.enar.picture.Model;
+import co.edu.uniandes.picture.webpicture.metamodel.Metamodel;
+
+/**
+ * Valida que el contenido del .ecore y el .picture correspondan 
+ * @author hellspawn
+ */
+public class Validator 
+{
+	//------------------------------------------------------------------
+	//Atributos
+	//------------------------------------------------------------------
+
+	//------------------------------------------------------------------
+	//Constructores
+	//------------------------------------------------------------------
+
+	//------------------------------------------------------------------
+	//Metodos
+	//------------------------------------------------------------------
+
+	/**
+	 * Valida que el contenido del archivo .picture y el .ecore correspondan 
+	 * @param model - Es el modelo generado a partir del picture
+	 * @param metamodel - Es el metamodelo construido a partir del .ecore
+	 * @return true/false
+	 */
+	public boolean validate(Model model, Metamodel metamodel)
+	{
+		boolean ans = false;
+		return ans;
+	}
+	
+	//------------------------------------------------------------------
+	//Getters & Setters
+	//------------------------------------------------------------------
+	
+	
+}
