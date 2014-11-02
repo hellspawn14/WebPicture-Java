44e13e45c993f1f569099190b55321f484a73b13
hellspawn14 <hellspawn@192.168.0.26>
2014-11-02 06:50:17 -0500

    KK

--- co.edu.uniandes.picture.webpicture/src/co/edu/uniandes/picture/webpicture/graphical/shapes/Graph.java
+++ co.edu.uniandes.picture.webpicture/src/co/edu/uniandes/picture/webpicture/graphical/shapes/Graph.java
@@ -40,6 +40,11 @@ public abstract class Graph
 	 */
 	private String shapeId;
 	
+	/**
+	 * Nombre del metaelemento que representa
+	 */
+	private String metaelementName;
+	
 	//------------------------------------------------------------------
 	//Constructores
 	//------------------------------------------------------------------
@@ -51,14 +56,16 @@ public abstract class Graph
 	 * @param x - Posición inicial en X
 	 * @param y - Posición inicial en Y
 	 * @param label - Label de la grafica 
+	 * @param metaelementName - Nombre del metaelemento que representa 
 	 */
-	public Graph(int height, int width, int x, int y, String label) 
+	public Graph(int height, int width, int x, int y, String label, String metaelementName) 
 	{
 		this.height = height;
 		this.width = width;
 		X = x;
 		Y = y;
 		this.label = label;
+		this.setMetaelementName(metaelementName);
 	}
 	
 	//------------------------------------------------------------------
@@ -157,6 +164,20 @@ public abstract class Graph
 	 */
 	public void setShapeId(String shapeId) {
 		this.shapeId = shapeId;
+	}
+
+	/**
+	 * @return the metaelementName
+	 */
+	public String getMetaelementName() {
+		return metaelementName;
+	}
+
+	/**
+	 * @param metaelementName the metaelementName to set
+	 */
+	public void setMetaelementName(String metaelementName) {
+		this.metaelementName = metaelementName;
 	}
 	
 	
