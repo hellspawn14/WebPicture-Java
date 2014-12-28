426b33786ef81bb3202bf6fdf05bc6c104835bd4
hellspawn14 <hellspawn@192.168.0.26>
2014-12-28 01:07:29 -0500

    Forma

---
+++ co.edu.uniandes.picture.webpicture/src/en/ar/picture/webpicture/graphical/elements/Shape.java
@@ -0,0 +1,70 @@
+package en.ar.picture.webpicture.graphical.elements;
+
+import en.ar.picture.webpicture.graphical.style.Border;
+import en.ar.picture.webpicture.graphical.style.Color;
+
+public abstract class Shape extends Graph{
+	// ------------------------------------------------------------------
+	// Atributos 
+	// ------------------------------------------------------------------
+
+	/**
+	 * Color del elemento
+	 */
+	private Color color;
+	
+	/**
+	 * Borde del elemento 
+	 */
+	private Border border;
+	
+	// ------------------------------------------------------------------
+	// Constructores 
+	// ------------------------------------------------------------------
+
+	/**
+	 * Crea una nueva forma para representar el elemento 
+	 * @param height - Alto del elemento
+	 * @param width - Ancho del elemento
+	 * @param type - Clase representada
+	 * @param label - Etiqueta/Texto del elemento
+	 * @param labelPlacement - Posición del texto
+	 * @param phantom - Indicador de transparencia
+	 */
+	public Shape(int height, int width, String type, String label, String labelPlacement, boolean phantom)
+	{
+		super(height, width, type, label, labelPlacement, phantom);
+	}
+
+	// ------------------------------------------------------------------
+	// Getters & Setters 
+	// ------------------------------------------------------------------
+
+	/**
+	 * @return the color
+	 */
+	public Color getColor() {
+		return color;
+	}
+
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	/**
+	 * @return the border
+	 */
+	public Border getBorder() {
+		return border;
+	}
+
+	/**
+	 * @param border the border to set
+	 */
+	public void setBorder(Border border) {
+		this.border = border;
+	}
+}
