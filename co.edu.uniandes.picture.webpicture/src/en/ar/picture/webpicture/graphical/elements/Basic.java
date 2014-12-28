f4b7fa9a4669c96d35cb410cf24fbb0cbd74ba4a
hellspawn14 <hellspawn@192.168.0.26>
2014-12-28 01:17:55 -0500

    Basic

--- co.edu.uniandes.picture.webpicture/src/en/ar/picture/webpicture/graphical/elements/Basic.java
+++ co.edu.uniandes.picture.webpicture/src/en/ar/picture/webpicture/graphical/elements/Basic.java
@@ -1,5 +1,8 @@
 package en.ar.picture.webpicture.graphical.elements;
 
+import en.ar.picture.webpicture.graphical.style.Border;
+import en.ar.picture.webpicture.graphical.style.Color;
+
 /**
  * Representa la especificacion para construir una forma
  * Las formas basicas no tienen una imagen 
@@ -11,15 +14,17 @@ public abstract class Basic extends Shape{
 	// ------------------------------------------------------------------
 
 	/**
-	 * Crea una nueva forma basica para representar el elemento 
+	 * Crea un elemento basico para representar el elemento 
 	 * @param height - Alto del elemento
 	 * @param width - Ancho del elemento
 	 * @param type - Clase representada
 	 * @param label - Etiqueta/Texto del elemento
 	 * @param labelPlacement - Posición del texto
 	 * @param phantom - Indicador de transparencia
+	 * @param color - Color de fondo del elemento 
+	 * @param border - Borde del elemento
 	 */
-	public Basic(int height, int width, String type, String label, String labelPlacement, boolean phantom) {
-		super(height, width, type, label, labelPlacement, phantom);
+	public Basic(int height, int width, String type, String label, String labelPlacement, boolean phantom, Color color, Border border) {
+		super(height, width, type, label, labelPlacement, phantom, color, border);
 	}
 }
