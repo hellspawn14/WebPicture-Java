93e8d1d02a1260c4f7b4941ab319b6bb4dc0d531
hellspawn14 <hellspawn@192.168.0.26>
2015-01-04 14:50:30 -0500

    Ya pasa a la vista de errores

--- co.edu.uniandes.picture.webpicture/src/en/ar/picture/webpicture/servlets/NewEditorServlet.java
+++ co.edu.uniandes.picture.webpicture/src/en/ar/picture/webpicture/servlets/NewEditorServlet.java
@@ -2,6 +2,7 @@ package en.ar.picture.webpicture.servlets;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Iterator;
 import java.util.List;
 
@@ -75,6 +76,11 @@ public class NewEditorServlet extends HttpServlet {
 	 * Directorio del editor
 	 */
 	private String dir;
+	
+	/**
+	 * Flujo de datos para enviar la respuesta
+	 */
+	private PrintWriter out;
 
 	// ------------------------------------------------------------------
 	// Inicialización
@@ -102,7 +108,23 @@ public class NewEditorServlet extends HttpServlet {
 		fileManager = webpicture.getFileManager(request.getServletContext().getRealPath("/"));
 		dir = fileManager.makeDir();
 		getParameters(request, dir);
-		webpicture.createEditor(dName, dDescription, dAuthor, dir);
+		
+		response.setContentType("text/html");
+		out = response.getWriter();
+		
+		try
+		{
+			webpicture.createEditor(dName, dDescription, dAuthor, dir);
+		}
+		catch(Exception e)
+		{
+			
+			String error = "<! DOCTYPE html><meta charset='UTF-8'><html lang='en'><link rel='icon' href='resources/res/siteIcon.png' type='image/x-icon'><meta name='viewport' content='width=device-width, initial-scale=1.0'><meta name='description' content='Index web site for WebPicture online DSL model editor'><head><title>WebPicture</title><link rel='icon' href='resources/res/siteIcon.png' type='image/x-icon'><script src='http://code.jquery.com/jquery-2.1.1.js'></script><script src='http://code.jquery.com/ui/jquery-ui-git.js'></script><script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js'></script><link rel='stylesheet' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'><script src='resources/bootbox/bootbox.js'></script><link rel='stylesheet' href='http://yui.yahooapis.com/pure//pure.css'><link rel='stylesheet' href='http://yui.yahooapis.com/pure/0.5.0/pure-min.css'><link rel='stylesheet' href='http://yui.yahooapis.com/pure//grids-responsive.css'><link rel='stylesheet' href='resources/pure-css/css/layouts/marketing.css'><link rel='stylesheet' href='http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css'><link rel='stylesheet' href='resources/pure-css/css/layouts/headers.css'></head><body><div class='header'><div class='home-menu pure-menu pure-menu-open pure-menu-horizontal pure-menu-fixed' style='background-color:#181818'><a class='pure-menu-heading' href='welcome'><img src='resources/res/logo.png' class='pure-img' /></a></div></div><div class='pure-g'><div class='pure-u-1-2' style='text-align:center;border-right:1px solid #808080; height:100%; background-color: #F0F0F0 ;'><div class='hero-titles'><h3 class='hero-tagline' style='text-align:center; margin-top:50px'>Upps there has been an error </h3><img class='pure-img-responsive' src='resources/res/errorReport.png' style='width:auto;height:auto;text-align:center ' /></div></div><div class='pure-u-1-2' style='text-align:center; height:100%; overflow-y: auto'><div class='hero-titles'><h3 class='hero-tagline' style='text-align:center; margin-top:50px'>Error report</h3></div><div style='margin-top:30%'><h3 class='marketing-header'>" + e.getMessage() + "</h3><a href='new_editor' class='button-cta pure-button'>Return</a></div></div></div><div class='footer l-box is-center' style='background-color:#181818; margin-top:0px'><p>All rights reserved Uniandes 2014</p><p>Universidad de los Andes - Computer engineering department - Engineering Faculty</p></div></body></html>";
+			out.println(error);
+			out.close();
+			//e.printStackTrace();
+		}
+		
 	}
 
 	/**
