9d591e6b999ac9d833ca56b544865a17dcdc3f15
hellspawn14 <hellspawn@192.168.0.26>
2015-01-04 23:10:44 -0500

    NAdsa

--- co.edu.uniandes.picture.webpicture/src/en/ar/picture/webpicture/graphical/rules/estructural/connection/LinkingRulesGenerator.java
+++ co.edu.uniandes.picture.webpicture/src/en/ar/picture/webpicture/graphical/rules/estructural/connection/LinkingRulesGenerator.java
@@ -39,11 +39,8 @@ public class LinkingRulesGenerator {
 		{
 			for (int k = 0; k < metamodel.getModelElements().get(i).getReferences().size(); k++)
 			{
-				if (metamodel.getModelElements().get(i).getReferences().get(k).getGrpLink() != null)
-				{
-					LinkingRuleDefinition L = new LinkingRuleDefinition(metamodel.getModelElements().get(i).getReferences().get(k));
-					getLinkingRules().add(L);
-				}
+				LinkingRuleDefinition L = new LinkingRuleDefinition(metamodel.getModelElements().get(i).getReferences().get(k));
+				getLinkingRules().add(L);	
 			}
 		}
 	}
