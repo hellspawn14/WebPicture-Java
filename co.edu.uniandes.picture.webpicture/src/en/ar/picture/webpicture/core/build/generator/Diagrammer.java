fc4965581e9864ee8ac0ad2def4d48a230a17dc8
hellspawn14 <hellspawn@192.168.0.26>
2015-01-08 05:34:36 -0500

    Genera cosas cheveres

---
+++ co.edu.uniandes.picture.webpicture/src/en/ar/picture/webpicture/core/build/generator/Diagrammer.java
@@ -0,0 +1,5 @@
+package en.ar.picture.webpicture.core.build.generator;
+
+public class Diagrammer {
+
+}
---
+++ co.edu.uniandes.picture.webpicture/src/en/ar/picture/webpicture/graphical/paper/Paper.java
@@ -0,0 +1,70 @@
+package en.ar.picture.webpicture.graphical.paper;
+
+import en.ar.picture.webpicture.graphical.rules.estructural.connection.LinkingRulesGenerator;
+
+/**
+ * Definicion del paper principal para generar el script 
+ * @author hellspawn
+ */
+public class Paper {
+	// ------------------------------------------------------------------
+	// Constantes 
+	// ------------------------------------------------------------------
+
+	// ------------------------------------------------------------------
+	// Atributos 
+	// ------------------------------------------------------------------
+
+	/**
+	 * Reglas de validacion de conexion
+	 */
+	private LinkingRulesGenerator validationRules;
+
+	
+	// ------------------------------------------------------------------
+	// Constructores 
+	// ------------------------------------------------------------------
+
+	/**
+	 * Crea una nueva definición para el paper principal 
+	 * @param validationRules - Reglas validación de conexion 
+	 */
+	public Paper (LinkingRulesGenerator validationRules)
+	{
+		this.validationRules = validationRules;
+	}
+	
+	// ------------------------------------------------------------------
+	// Metodos 
+	// ------------------------------------------------------------------
+
+	/**
+	 * Genera el script del paper principal con las reglas de validacion 
+	 * @return - Codigo JS para crear el paper principal 
+	 */
+	public String generateScript()
+	{
+		String ans = "var graph = new joint.dia.Graph;var paper = new joint.dia.Paper({ el: $('#modelCanvas'), gridSize: 10,height: $('#modelCanvas').height(),width: $('#modelCanvas').width(),gridSize: 1,model: graph,";
+		ans += validationRules.generateLinkingRulesScript();
+		ans += "});";
+		return ans; 
+	}
+	
+	// ------------------------------------------------------------------
+	// Getters & Setters 
+	// ------------------------------------------------------------------
+	
+	/**
+	 * @return the validationRules
+	 */
+	public LinkingRulesGenerator getValidationRules() {
+		return validationRules;
+	}
+
+	/**
+	 * @param validationRules the validationRules to set
+	 */
+	public void setValidationRules(LinkingRulesGenerator validationRules) {
+		this.validationRules = validationRules;
+	}
+}
